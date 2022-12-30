5d502c05893cf1526f36a0d4935befe391b95403
suk13574 <suk13574@naver.com>
2022-12-30 14:36:53 +0900

    [수정] JPQL에서 조건식 사용하기
    조건식
    1. CASE식: 조건을 설정하거나 하나의 값에 따른 반환 가능
    2. coalesce식: 하나씩 조회해서 null이 아니면 지정한 값 반환
    3. nullif식: 두 값이 같으면 null 반환

--- study/src/main/java/jpabasic/hellojpa/JpaMain.java
+++ study/src/main/java/jpabasic/hellojpa/JpaMain.java
@@ -23,7 +23,7 @@ public class JpaMain {
             em.persist(team);
 
             MemberTest member = new MemberTest();
-            member.setUsername("member1");
+            member.setUsername("관리자");
             member.setAge(10);
             member.changeTeam(team);
             member.setType(MemberType.ADMIN);
@@ -32,16 +32,24 @@ public class JpaMain {
             em.flush();
             em.clear();
 
-            String query = "select m.username, 'HELLO', TRUE From MemberTest m " +
-                    "Where m.type = :userType";
-            List<Object[]> result = em.createQuery(query)
-                    .setParameter("userType", MemberType.ADMIN)
-                    .getResultList();
+            //case 문 사용해보기
+//            String query =
+//                    "select " +
+//                            "case when m.age <= 10 then '학생요금' " +
+//                            " when m.age >= 60 then '경로요금' " +
+//                            " else '일반요금' END " +
+//                            "from MemberTest m ";
 
-            for (Object[] objects : result) {
-                System.out.println("objects = " + objects[0]);
-                System.out.println("objects = " + objects[1]);
-                System.out.println("objects = " + objects[2]);
+
+            // coalesce 사용해보기
+//            String query = "select coalesce(m.username, '이름 없는 회원') from MemberTest m";
+
+            String query = "select NULLIF(m.username, '관리자') from MemberTest m";
+
+            List<String> result = em.createQuery(query, String.class)
+                    .getResultList();
+            for (String s : result) {
+                System.out.println("s = " + s);
             }
 
             tx.commit();
