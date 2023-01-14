73288eb730f7fe63160bcc7eca128c2c70b43cf7
suk13574 <suk13574@naver.com>
2023-01-14 11:40:09 +0900

    [수정] JPQL 표준 함수인 size 사용해보기
    JPQL에서 제공하는 표준 함수는 DB 상관없이 사용 가능
    문자함수: CONCAT, SUBSTRING, TRIM, LOWER, UPPER, LENGTH, LOCATE
    수학 함수: ABS, SQRT, SIZE, INDEX
    날짜 함수: CURRENT_DATE, CURRENT_TIME, CURRENT_TIMESTAMP

--- study/src/main/java/jpabasic/hellojpa/JpaMain.java
+++ study/src/main/java/jpabasic/hellojpa/JpaMain.java
@@ -18,37 +18,25 @@ public class JpaMain {
 
         try{
 
-            Team team = new Team();
-            team.setName("team1");
-            em.persist(team);
+//            Team team = new Team();
+//            team.setName("team1");
+//            em.persist(team);
 
             MemberTest member = new MemberTest();
             member.setUsername("관리자");
-            member.setAge(10);
-            member.changeTeam(team);
-            member.setType(MemberType.ADMIN);
+//            member.setAge(10);
+//            member.changeTeam(team);
+//            member.setType(MemberType.ADMIN);
             em.persist(member);
 
             em.flush();
             em.clear();
 
-            //case 문 사용해보기
-//            String query =
-//                    "select " +
-//                            "case when m.age <= 10 then '학생요금' " +
-//                            " when m.age >= 60 then '경로요금' " +
-//                            " else '일반요금' END " +
-//                            "from MemberTest m ";
+            String query = "select size(t.members) from Team t";
 
-
-            // coalesce 사용해보기
-//            String query = "select coalesce(m.username, '이름 없는 회원') from MemberTest m";
-
-            String query = "select NULLIF(m.username, '관리자') from MemberTest m";
-
-            List<String> result = em.createQuery(query, String.class)
+            List<Integer> result = em.createQuery(query, Integer.class)
                     .getResultList();
-            for (String s : result) {
+            for (Integer s : result) {
                 System.out.println("s = " + s);
             }
 
